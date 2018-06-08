8e8f122183e021d8e9e6d46c7145e1e5d8de2592
Michelle Olivieri <michelleo72@gmail.com>
2018-06-08 16:26:25 +0000

    automatically backed up by learn

--- app/helpers/artists_helper.rb
+++ app/helpers/artists_helper.rb
@@ -1,10 +1,10 @@
 module ArtistsHelper
 
   def display_artist(song)
-    if song.artist.try(:name).blank?
-      link_to "Add Artist", edit_song_path(song)
-    else
+    if song.artist
       link_to song.artist_name, artist_path(song.artist)
+    else
+      link_to "Add Artist", edit_song_path(song)
     end
   end
 end
