34f07bd719245be19315dafe7d634fad20d3ab0e
Michelle Olivieri <michelleo72@gmail.com>
2018-06-08 15:32:27 +0000

    automatically backed up by learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -10,12 +10,4 @@ class Song < ActiveRecord::Base
     # self.artist.update(artist)
   end
 
-    def category_name=(name)
-  self.category = Category.find_or_create_by(name: name)
-end
-
-def category_name
-   self.category ? self.category.name : nil
-end
-  
 end
