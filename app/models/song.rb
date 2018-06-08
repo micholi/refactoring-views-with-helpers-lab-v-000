e82ee62175465f1bc70cb99759d8005d69ba68e6
Michelle Olivieri <michelleo72@gmail.com>
2018-06-08 15:32:20 +0000

    automatically backed up by learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -2,17 +2,13 @@ class Song < ActiveRecord::Base
   belongs_to :artist
 
   def artist_name
+    self.artist ? self.artist.name :nil
   end
 
   def artist_name=(name)
     self.artist = Artist.find_or_create_by(name: artist.name)
     # self.artist.update(artist)
   end
-
-  def artist_attributes=(artist)
-      self.artist = Artist.find_or_create_by(name: artist.name)
-      self.artist.update(artist)
-    end
 
     def category_name=(name)
   self.category = Category.find_or_create_by(name: name)
